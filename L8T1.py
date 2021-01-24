2e9df4d21f5a35f0b8a7e2350cb5be7466526d38
KindFest <kindfey@ya.ru>
2021-01-24 22:12:11 +0300

    Lesson 8

---
+++ L8T1.py
@@ -0,0 +1,49 @@
+# Урок 8. Практическое задание 1.
+# ФИО: Артур Назарян
+# Курс: Алгоритмы и структуры данных на Python. Базовый курс
+# Факультет: Geek University Python-разработки
+#
+# 1) Определение количества различных подстрок с использованием хеш-функции. Пусть на вход функции
+# дана строка. Требуется вернуть количество различных подстрок в этой строке.
+# Примечание: в сумму не включаем пустую строку и строку целиком.
+# Пример работы функции:
+#
+# func("papa")
+# 6
+# func("sova")
+# 9
+import hashlib
+
+
+def substr_count_1(my_str):
+    """
+    Решение с использованием hash функции
+    """
+    result = []
+    i = len(my_str)
+    while i > 0:
+        lenght = len(my_str)
+        for j in range(lenght - i + 1):
+            if hashlib.sha256(my_str[j:j + i].encode('utf-8')).hexdigest() not in result:
+                result.append(hashlib.sha256(my_str[j:j + i].encode('utf-8')).hexdigest())
+        i -= 1
+    return len(result) - 1
+
+
+def substr_count_2(my_str):
+    """
+    Решение с использованием set (ибо set использует hash)
+    """
+    result = set()
+    i = len(my_str)
+    while i > 0:
+        lenght = len(my_str)
+        for j in range(lenght - i + 1):
+            result.add(my_str[j:j + i])
+        i -= 1
+    return len(result) - 1
+
+
+my_string = 'sova'
+print(f'Количество уникальных подстрок в строке {my_string} = {substr_count_1(my_string)}')
+print(f'Количество уникальных подстрок в строке {my_string} = {substr_count_2(my_string)}')
